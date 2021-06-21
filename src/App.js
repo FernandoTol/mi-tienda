b89a140e6d90aeb17dae0be59dcc6ed270ff26ec
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-06-21 10:29:33 -0500

    requerimiento de la entrega 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
-
-import  "bootstrap/dist/css/bootstrap.min.css";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import Navbar from "./components/NavbarComponent/Navbar";
 
 
 function App() {
   return (
+    <div className="App">
     <Navbar/>
+    <ItemListContainer greeting = {'Hola buenas tardes como estan'}/>
+    </div>
   );
 }
 
