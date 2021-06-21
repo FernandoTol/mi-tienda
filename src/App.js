34fc504a9265d0546285965fb41150b6fcf9718c
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-06-21 09:35:07 -0500

    re asignacion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+import  "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/NavbarComponent/Navbar";
+
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ src/components/NavbarComponent/Navbar.js
@@ -0,0 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+
+export const Navbar = () => {
+    return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Babidas</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Desayunos</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Comidas</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Cenas</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    )
+}
+
+export default Navbar
