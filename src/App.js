4dacf58fee5e6e19f2ce19174d6b4293c73b7708
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-06-21 13:30:42 -0500

    entrega 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+import { CardComponent } from "./components/CardItem/CardItem";
+// import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import Navbar from "./components/NavbarComponent/Navbar";
+
 
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
-    <ItemListContainer greeting = {'Hola buenas tardes como estan'}/>
+    {/* <ItemListContainer greeting = {'Hola buenas tardes como estan'}/> */}
+    <CardComponent/>
     </div>
   );
 }
