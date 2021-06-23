89889624232db94ff180900f6886e7a8a8d97d01
FernandoTol <60888678+FernandoTol@users.noreply.github.com>
2021-06-22 19:18:05 -0500

    acomodo

--- src/components/itemCount/itemCount.jsx
+++ src/components/itemCount/itemCount.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
 
 
@@ -6,21 +6,21 @@ export const ItemCount = (props) => {
 
     const [valor, setValor] = useState(props.initial);
 
-    function onAdd () {
-        if(valor < props.stock) {
-        setValor (parseInt(valor) +1)
+    function onAdd() {
+        if (valor < props.stock) {
+            setValor(parseInt(valor) + 1)
         };
     };
 
-    function onSubstract () {
-        if(valor > 1 ) {
-            setValor (parseInt(valor) -1)
+    function onSubstract() {
+        if (valor > 1) {
+            setValor(parseInt(valor) - 1)
         } else {
             alert('La cantidad no puede ser menor que 1')
         }
     };
 
-    return(
+    return (
         <div>
             <button className="btn btn-secondary" onClick={onSubstract}> - </button>
             <input type="text" value={valor}></input>
